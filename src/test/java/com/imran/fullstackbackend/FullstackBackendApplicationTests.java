bf9f292b0b45c12b319f9090249d61f7de7d5bbb
imranKhanYRT <imran.khan.yrt@gmail.com>
2023-10-14 21:41:32 +0600

    initial setup spring initializer

---
+++ src/test/java/com/imran/fullstackbackend/FullstackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imran.fullstackbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
