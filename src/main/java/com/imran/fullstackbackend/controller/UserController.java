3d04b1f51d7e6d9fdeb6e1ace6f12a22f94601bf
imranKhanYRT <imran.khan.yrt@gmail.com>
2023-10-15 11:00:47 +0600

    add user not found

---
+++ src/main/java/com/imran/fullstackbackend/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.imran.fullstackbackend.controller;
+
+import com.imran.fullstackbackend.exception.UserNotFoundException;
+import com.imran.fullstackbackend.model.User;
+import com.imran.fullstackbackend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/user")
+    User newUser(@RequestBody User newUser){
+        return  userRepository.save(newUser);
+    }
+
+    @GetMapping("/users")
+    List<User> getUser(){
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/user/{id}")
+    User getUserById(@PathVariable Long id){
+        return  userRepository.findById(id)
+                .orElseThrow(()-> new UserNotFoundException(id));
+    }
+}
